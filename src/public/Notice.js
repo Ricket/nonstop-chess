a1eb24436b28ceddbb63f313700b240626c7e213
Richard Carter <richard@richardcarter.org>
2013-04-25 12:48:47 -0400

    Notice: isVisible

--- src/public/Notice.js
+++ src/public/Notice.js
@@ -7,17 +7,24 @@ this.Notice = (function () {
     var exports = {};
 
     var notice = document.getElementById("notice"),
-        mask = document.getElementById("mask");
+        mask = document.getElementById("mask"),
+        isVisible = true;
+
+    exports.isVisible = function () {
+        return isVisible;
+    };
 
     exports.show = function (text) {
         notice.innerHTML = text;
         notice.style.display = null;
         mask.style.display = null;
+        isVisible = true;
     };
 
     exports.hide = function () {
         notice.style.display = "none";
         mask.style.display = "none";
+        isVisible = false;
     };
 
     return exports;
